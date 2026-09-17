87444a9a99bd32853a81160da639628e8f8cfcd4
jonaskart <jonas.kartbraten@gmail.com>
2026-09-17 17:31:23 +0200

    added files from previous session; starting on typing script

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 //blinking thing
-const command = "cat cv.txt"
-
 var blink_speed = 500; // every 1000 == 1 second, adjust to suit
 var t = setInterval(function () {
     var ele = document.getElementById('blinking');
     ele.style.visibility = (ele.style.visibility == 'hidden' ? '' : 'hidden');
 }, blink_speed);
 
-document.getElementById("console").onkeydown = function() {myFunction()};
+/*
 
-function myFunction() {
-  document.getElementById('hiddencommand').innerHTML = command;
+const command = "cat cv.txt";
+
+let command = "cat cv.txt";
+let advanced = 0;
+
+document.addEventListener('keydown', (event) => {
+  advanced = advanced + 1;
+  advance();
+});
+
+
+function advance() {
+  if (advanced >= command.length) {
+    console.log("nuh uh");
+  }
 }
+
+*/
